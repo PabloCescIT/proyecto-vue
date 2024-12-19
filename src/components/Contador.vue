2fe6192831486f00555d08967cbeb7e7b8b63b23
Pablo de Maya <pablo.cescit@gmail.com>
2024-12-19 11:34:26 +0100

    Creación del componente "Contador"

---
+++ src/components/Contador.vue
@@ -0,0 +1,23 @@
+<template>
+ <h1>Contador:</h1>
+ <p>{{ contador }}</p>
+ <button @click="sumar">Sumar</button>
+ <button @click="restar">Restar</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const contador = ref(0);
+const sumar = () => {
+  contador.value++;
+}
+const restar = () => {
+  contador.value--;
+}
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
