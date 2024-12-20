2f5d7d1736e8a3e4ae3b6b970896368d6987f736
Pablo de Maya <pablo.cescit@gmail.com>
2024-12-20 16:21:28 +0100

    Uso de "Computed Properties", creación de una calculadora que multiplica por 3 y uso de v-show y v-if

---
+++ src/modules/calcular/views/CalcularView.vue
@@ -0,0 +1,25 @@
+<template>
+    <h3>Propiedades computadas</h3>
+    <input type="text" v-model="valor">
+    {{ valorCalculado }}
+    <ResultadoSection v-if="mostrarElemento" :resultado="valorCalculado"></ResultadoSection>
+    <div v-else-if="isNaN(valor)">Debe de ser un número para poder hacer los cálculos</div>
+    <div v-else>Aquí no se muestra nada superior a la edad de Jesucristo</div>
+</template>
+
+<script setup>
+import {ref, computed} from 'vue';
+import ResultadoSection from './components/ResultadoSection.vue';
+const valor = ref('');
+const valorCalculado = computed(()=> {
+    return valor.value * 3
+});
+
+const mostrarElemento = computed(()=> {
+    return valor.value > 10;
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    /* {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    */
+
     {
       path: '/contador',
       name: 'copntador',
@@ -24,8 +19,14 @@ const router = createRouter({
       path: '/registrar',
       name: 'registrar',
       component: () => import('../modules/registro/views/RegistrarView.vue')
+    },
+    {
+      path: '/calcular',
+      name: 'calcular',
+      component: () => import('../modules/calcular/views/CalcularView.vue')
     }
   ]
+
 })
 
 export default router
